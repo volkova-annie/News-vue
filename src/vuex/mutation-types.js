dbf0aeb9e19eda6925813385297078baaf2e841b
Anna Volkova <annie_v@bk.ru>
2017-08-13 22:44:41 +0300

    remove extra files

--- src/vuex/mutation-types.js
+++
@@ -1,4 +0,0 @@
-export const UPDATE_NEWS = 'UPDATE_NEWS'
-export const REQUEST_NEWS = 'REQUEST_NEWS'
-export const SORT_NEWS = 'SORT_NEWS'
-export const DELETE_NEWS = 'DELETE_NEWS'
--- src/vuex/utils/api.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-export default {
-    const apiKey = 'ca97653137944f2fac0abe57e6a6d7e0';
-    const url = 'https://newsapi.org/v1/articles?source=hacker-news&apiKey='+apiKey;
-
-        return Vue.http.get(url, request)
-
-}
